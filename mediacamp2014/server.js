8040c7d83065ecce30cf85ebe321f902563257e1
Sebastian Otto <sotto@wemakeweb.de>
2014-12-01 17:08:11 +0100

    added server file

---
+++ mediacamp2014/server.js
@@ -0,0 +1,27 @@
+var _ = require('underscore');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendfile('files.zip');
+});
+
+io.on('connection', function(socket){
+        socket.on('message', function(msg){
+            var msgObj = {createdAt: new Date()};
+            if(_.isString(msg)){
+                    msgObj.username = "Unknown";
+                    msgObj.message = msg;
+            } else {
+                    msgObj = msg;
+                    msgObj.createdAt = new Date();
+            }
+            console.log(msgObj);
+            io.emit('message', msgObj);
+        });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
