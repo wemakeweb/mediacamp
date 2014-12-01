06ab56c39688e45addf75ca08b2e46bb4450cee9
Sebastian Otto <sotto@wemakeweb.de>
2014-12-01 17:09:34 +0100

    remove unsed file serving code

--- mediacamp2014/server.js
+++ mediacamp2014/server.js
@@ -3,10 +3,6 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendfile('files.zip');
-});
-
 io.on('connection', function(socket){
         socket.on('message', function(msg){
             var msgObj = {createdAt: new Date()};
